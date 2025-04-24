89741509d98307ebafd89fb093cab19fe753a64f
Kwame Oduro <odurokwameee@gmail.com>
2025-04-24 19:42:24 +0000

    add: An Invalid Salary Exception

---
+++ src/exceptions/InvalidSalaryException.java
@@ -0,0 +1,11 @@
+package exceptions;
+
+/**
+ * Exception thrown when an invalid salary value is provided.
+ */
+public class InvalidSalaryException extends RuntimeException {
+
+    public InvalidSalaryException(String message) {
+        super(message);  // Keeps exception functionality but removes log clutter
+    }
+}

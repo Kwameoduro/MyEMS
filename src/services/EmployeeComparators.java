a378d40817786e5c5e7f1369a86baa1c7cffb48a
Kwame Oduro <odurokwameee@gmail.com>
2025-04-24 19:28:38 +0000

    update: Employee Comparator file

--- src/services/EmployeeComparators.java
+++ src/services/EmployeeComparators.java
@@ -4,9 +4,9 @@ import java.util.Comparator;
 
 public class EmployeeComparators {
     //Comparator to sort employees by salary in descending order
-    public static class EmployeeSalaryComparator<T> implements Comparator<Employee<T>> {
+    public static class EmployeeSalaryComparator<T> implements Comparator<model.Employee<T>> {
         @Override
-        public int compare(Employee<T> e1, Employee<T> e2){
+        public int compare(model.Employee<T> e1, model.Employee<T> e2){
             if(e1 == null || e2 == null){
                 throw new IllegalArgumentException("Cannot compare null employees");
             }
@@ -15,17 +15,17 @@ public class EmployeeComparators {
     }
 
     //Comparator to sort employees by performance rating in descending order
-    public static class EmployeePerformanceComparator<T> implements Comparator<Employee<T>>{
+    public static class EmployeePerformanceComparator<T> implements Comparator<model.Employee<T>>{
         @Override
-        public int compare(Employee<T> e1, Employee<T> e2){
+        public int compare(model.Employee<T> e1, model.Employee<T> e2){
             return Double.compare(e2.getPerformanceRating(), e1.getPerformanceRating());
         }
     }
 
     //Comparator to sort employees by years of experience
-    public static class EmployeeExperienceComparator<T> implements Comparator<Employee<T>>{
+    public static class EmployeeExperienceComparator<T> implements Comparator<model.Employee<T>>{
         @Override
-        public int compare(Employee<T> e1, Employee<T> e2){
+        public int compare(model.Employee<T> e1, model.Employee<T> e2){
             return Integer.compare(e2.getYearsOfExperience(), e1.getYearsOfExperience());
         }
     }

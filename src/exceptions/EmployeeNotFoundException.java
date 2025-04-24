66c102a8416bd22559d9abd800d25e8110fa9f38
Kwame Oduro <odurokwameee@gmail.com>
2025-04-24 19:41:26 +0000

    add: An Employee not found exception

---
+++ src/exceptions/EmployeeNotFoundException.java
@@ -0,0 +1,11 @@
+package exceptions;
+
+/**
+ * Exception thrown when an employee is not found in the database.
+ */
+public class EmployeeNotFoundException extends RuntimeException {
+
+    public EmployeeNotFoundException(String message) {
+        super(message);  // Exception is properly thrown without logging clutter
+    }
+}
